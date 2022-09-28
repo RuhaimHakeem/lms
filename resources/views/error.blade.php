cbb222149e2031f7264ea901e5cc7f5dcc460630
unknown <naashiknazar@gmail.com>
2022-09-28 08:50:16 +0530

    title name change

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <base href="../../../">
-    <title>ERROR</title>
+    <title>EDGE [Admin] Error</title>
     <meta charset="utf-8" />
     <meta name="description"
         content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Blazor, Django, Flask &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
