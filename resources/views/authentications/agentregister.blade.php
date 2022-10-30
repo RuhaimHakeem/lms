6bee267454d8e07321e276a6ce818964ab8be159
unknown <naashiknazar@gmail.com>
2022-10-30 16:32:52 +0530

    alter agentregister page

--- resources/views/authentications/agentregister.blade.php
+++ resources/views/authentications/agentregister.blade.php
@@ -185,56 +185,8 @@ License: For each use you must have a valid license purchased only from above li
                                     </div>
                                 </div>
                                 <!--begin::User account menu-->
-                                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px"
+                                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-150px"
                                     data-kt-menu="true">
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3">
-                                        <div class="menu-content d-flex align-items-center px-3">
-                                            <!--begin::Avatar-->
-                                            <div class="symbol symbol-50px me-5">
-                                                <img alt="Logo" src="../media/logos/profile2.png" />
-                                            </div>
-                                            <!--end::Avatar-->
-                                            <!--begin::Username-->
-                                            <div class="d-flex flex-column">
-                                                <div class="fw-bold d-flex align-items-center fs-5">Name
-                                                    <span
-                                                        class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span>
-                                                </div>
-
-                                            </div>
-                                            <!--end::Username-->
-                                        </div>
-                                    </div>
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu separator-->
-                                    <div class="separator my-2"></div>
-                                    <!--end::Menu separator-->
-                                    <!--begin::Menu item-->
-
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu item-->
-
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu item-->
-
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu item-->
-
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu separator-->
-                                    <div class="separator my-2"></div>
-                                    <!--end::Menu separator-->
-                                    <!--begin::Menu item-->
-
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-5 my-1">
-                                        <a href="#" class="menu-link px-5">Account
-                                            Settings</a>
-                                    </div>
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu item-->
                                     <div class="menu-item px-5">
                                         <form action="logout" method="POST">
                                             @csrf
